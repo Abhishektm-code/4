01f65500b5d7ad7534dd23f04902b502b3d550cc
abhishek <tmabhishek14@gmail.com>
2024-09-26 15:32:28 +0530

    Add files via upload

---
+++ even odd by array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[50],size=0,i;
+    printf("enter the size",size);
+    scanf("%d",&size);
+
+    printf("enter  the number:",&a[i]);
+    {
+    for(i=0; i<=size; i++)
+    {
+        {
+        scanf("%d",&a[i]);
+         }
+        if(a[i]%2==0)
+        {
+        printf("even number %d\n",a[i]);
+        }
+        else
+        {
+
+        printf("odd number %d\n",a[i]);
+        }
+    }
+    }
+}
+
